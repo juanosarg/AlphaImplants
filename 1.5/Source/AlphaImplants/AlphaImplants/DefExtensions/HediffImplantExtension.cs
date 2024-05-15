42d48bf0fe5f81b6c46ae9c74a1846f414507911
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-15 19:09:39 +0200

    v0.1

---
+++ 1.5/Source/AlphaImplants/AlphaImplants/DefExtensions/HediffImplantExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace AlphaImplants
+{
+    public class HediffImplantExtension : DefModExtension
+    {
+        public List<BodyPartGroupDef> bodyPartGroupsToMultiplyDamage;
+        public float powerMultiplier = 1;
+        public float cooldownTimeMultiplier = 1;
+
+
+
+    }
+}
---
+++ 1.5/Source/AlphaImplants/AlphaImplants/Harmony/Pawn_MeleeVerbs_GetUpdatedAvailableVerbsList.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace AlphaImplants
+{
+
+    [HarmonyPatch(typeof(HediffComp_VerbGiver))]
+    [HarmonyPatch("Tools", MethodType.Getter)]
+
+   
+    public static class AlphaImplants_HediffComp_VerbGiver_Tools_Patch
+    {
+        [HarmonyPostfix]
+        public static void MultiplyToolPower(HediffComp_VerbGiver __instance, List<Tool> __result)
+
+        {
+            if (__instance.parent.def.GetModExtension<HediffImplantExtension>() != null)
+            {
+                HediffImplantExtension extension = __instance.parent.def.GetModExtension<HediffImplantExtension>();
+                List<Tool> multipliedTools = new List<Tool>();
+                foreach (Tool tool in __result)
+                {
+                    Pawn pawn = __instance.parent.pawn;
+                    Tool originalTool = pawn.Tools.Where(x => extension.bodyPartGroupsToMultiplyDamage.Contains(x.linkedBodyPartsGroup)).RandomElement();
+
+
+                    Tool tool2 = tool;
+                    tool2.power = originalTool.power * extension.powerMultiplier;
+                    tool2.cooldownTime = originalTool.cooldownTime * extension.cooldownTimeMultiplier;
+                    multipliedTools.Add(tool2);
+
+                }
+                __result= multipliedTools;
+            }
+           
+
+        }
+
+       
+
+    }
+}
