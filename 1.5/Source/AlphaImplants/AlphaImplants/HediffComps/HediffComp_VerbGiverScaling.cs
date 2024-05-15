e98e19ca900ceee19e22e22654396708c14539c4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-15 08:53:58 +0200

    v0.1

--- 1.5/Source/AlphaImplants/AlphaImplants/HediffComps/HediffComp_VerbGiverScaling.cs
+++ 1.5/Source/AlphaImplants/AlphaImplants/HediffComps/HediffComp_VerbGiverScaling.cs
@@ -4,24 +4,18 @@ using RimWorld;
 using Verse;
 namespace AlphaImplants
 {
-    public class HediffComp_VerbGiverScaling : HediffComp, IVerbOwner
+    public class HediffComp_VerbGiverScaling : HediffComp_VerbGiver
     {
-        public VerbTracker verbTracker;
-
-        public HediffCompProperties_VerbGiverScaling Props => (HediffCompProperties_VerbGiverScaling)props;
-
-        public VerbTracker VerbTracker => verbTracker;
-
-        public List<VerbProperties> VerbProperties => Props.verbs;
+    
 
         public List<Tool> cachedTools;
 
 
-        public List<Tool> Tools {
+       public new List<Tool> Tools {
 
             get
             {
-                Log.Message("working");
+               
                 if(cachedTools == null)
                 {
                     foreach (Tool tool in Props.tools)
@@ -39,43 +33,6 @@ namespace AlphaImplants
             }
             
 
-        }
-
-        
-
-        Thing IVerbOwner.ConstantCaster => base.Pawn;
-
-        ImplementOwnerTypeDef IVerbOwner.ImplementOwnerTypeDef => Props.ownerTypeOverride ?? ImplementOwnerTypeDefOf.Hediff;
-
-        public HediffComp_VerbGiverScaling()
-        {
-            verbTracker = new VerbTracker(this);
-        }
-
-        public override void CompExposeData()
-        {
-            base.CompExposeData();
-            Scribe_Deep.Look(ref verbTracker, "verbTracker", this);
-            if (Scribe.mode == LoadSaveMode.PostLoadInit && verbTracker == null)
-            {
-                verbTracker = new VerbTracker(this);
-            }
-        }
-
-        public override void CompPostTick(ref float severityAdjustment)
-        {
-            base.CompPostTick(ref severityAdjustment);
-            verbTracker.VerbsTick();
-        }
-
-        string IVerbOwner.UniqueVerbOwnerID()
-        {
-            return parent.GetUniqueLoadID() + "_" + parent.comps.IndexOf(this);
-        }
-
-        bool IVerbOwner.VerbsStillUsableBy(Pawn p)
-        {
-            return p.health.hediffSet.hediffs.Contains(parent);
         }
     }
 }
