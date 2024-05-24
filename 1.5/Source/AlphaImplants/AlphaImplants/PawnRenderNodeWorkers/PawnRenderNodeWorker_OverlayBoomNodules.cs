0c3d438bb1d7ab834da5958777fc0cfeb947380c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-24 20:01:37 +0200

    v0.1

---
+++ 1.5/Source/AlphaImplants/AlphaImplants/PawnRenderNodeWorkers/PawnRenderNodeWorker_OverlayBoomNodules.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using Verse;
+namespace AlphaImplants
+{
+    public class PawnRenderNodeWorker_OverlayWounds : PawnRenderNodeWorker_Overlay
+    {
+        protected override PawnOverlayDrawer OverlayDrawer(Pawn pawn)
+        {
+            return new PawnBoomNodulesDrawer(pawn);
+        }
+
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (base.CanDrawNow(node, parms))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
