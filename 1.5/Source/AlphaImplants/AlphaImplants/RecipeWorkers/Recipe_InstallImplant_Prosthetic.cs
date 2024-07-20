ac2910241ee636c17ab42de92aca0912ecefb320
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-20 10:46:12 +0200

    v1.1

---
+++ 1.5/Source/AlphaImplants/AlphaImplants/RecipeWorkers/Recipe_InstallImplant_Prosthetic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace AlphaImplants
+{
+    public class Recipe_InstallImplant_Prosthetic : Recipe_InstallImplant {
+
+        public override AcceptanceReport AvailableReport(Thing thing, BodyPartRecord part = null)
+        {
+            Pawn pawn;
+            if ((pawn = thing as Pawn) == null)
+            {
+                return false;
+            }
+            Log.Message(pawn.RaceProps.baseBodySize);
+            if (pawn.RaceProps.baseBodySize < AlphaImplants_Settings.prostheticBodyLimit)
+            {
+                return false;
+            }
+            return base.AvailableReport(thing, part);
+        }
+
+
+
+
+    }
+
+
+}
+
